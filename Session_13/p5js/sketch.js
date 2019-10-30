b7c9fc930ba59b7985e15beb16fa7d1fbb670b5a
jeanettewells <jwells3@saic.edu>
2019-10-30 09:38:39 -0500

    update files

---
+++ Session_13/p5js/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  let canvas = createCanvas(400, 400);
+  canvas.parent("sketch");
+
+  background(250, 0, 250);
+}
+
+function draw() {
+  // put drawing code here
+
+  rect(100, 100, 50, 50);
+}
